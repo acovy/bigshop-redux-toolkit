9a350f51d4b87069cb47c48e68e01c9c49a813b8
Andrey <aseminmma@gmail.com>
2023-12-01 13:35:47 +0300

    добавил Sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from 'react';
+import styles from '../../styles/Sidebar.module.css';
+import { NavLink } from 'react-router-dom';
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <section className={styles.sidebar}>
+      <div className={styles.title}>
+        CATEGORIES
+      </div>
+      <nav>
+        <ul className={styles.menu}>
+          <li>
+            <NavLink to={`/categories/${1}`}>
+              Link
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+
+      <div className={styles.footer}>
+        <a 
+          href={"/help"} 
+          target='_blank' 
+          className={styles.link} rel="noreferrer" 
+        >
+          Help
+        </a>
+        <a 
+          href={"/terms"} 
+          target='_blank' 
+          className={styles.link} 
+          style={{textDecoration: "underline"}} 
+          rel="noreferrer"
+        >
+          Terms & Conditions
+        </a>
+      </div>
+    </section>
   )
 }
 
-export default Sidebar
+export default Sidebar;
