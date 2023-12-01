c9e7dcf662aa1a5995c5c33c4e2ac0ba985ccb7d
Andrey <aseminmma@gmail.com>
2023-12-01 15:05:13 +0300

    redux toolkit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from '../../styles/Header.module.css';
 import { Link } from 'react-router-dom';
 import { ROUTES } from '../../utils/routes';
-import LOGO from '../../images/logo.svg';
+import LOGO from '../../images/shop.svg';
 import AVATAR from '../../images/avatar.jpg';
 
 
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import styles from '../../styles/Sidebar.module.css';
 import { NavLink } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 const Sidebar = () => {
+
+  const { list } = useSelector(({categories}) => categories);
+
   return (
     <section className={styles.sidebar}>
       <div className={styles.title}>
@@ -10,11 +14,15 @@ const Sidebar = () => {
       </div>
       <nav>
         <ul className={styles.menu}>
-          <li>
-            <NavLink to={`/categories/${1}`}>
-              Link
+          {list.map(({id, name}) => (
+          <li key={id}>
+            <NavLink 
+              className={({isActive}) => `${styles.link} ${isActive ? styles.active : ""}`}
+              to={`/categories/${id}`}>
+              {name}
             </NavLink>
           </li>
+          ))}
         </ul>
       </nav>
 
